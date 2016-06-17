3fdea53c1054a11e28ae5ee0954fe60affd389e4
Denis Deliaev <cimmwolf@gmail.com>
2016-06-17 11:14:43 +0700

    added webcomponents

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
-var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
@@ -9,26 +8,52 @@ var autoprefixer = require('autoprefixer');
 var imagemin = require("gulp-imagemin");
 var newer = require('gulp-newer');
 var php2html = require('gulp-php2html');
+var vulcanize = require('gulp-vulcanize');
+var stylemod = require('gulp-style-modules');
+var polyclean = require('polyclean');
+var htmlmin = require('gulp-htmlmin');
+var cache = require('gulp-cached');
 
-gulp.task('default', ['scripts', 'css', 'images', 'fonts', 'html'], function() {
+gulp.task('default', ['components', 'scripts', 'css', 'images', 'fonts', 'html'], function () {
     gulp.src('dist/js/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('js/'));
+        .pipe(gulp.dest('dist/js/'));
     gulp.src('dist/css/*.css')
         .pipe(cssnano())
         .pipe(gulp.dest('dist/css'));
+    gulp.src([
+        'bower_components/app-*/*.html',
+        'bower_components/iron-*/*.html',
+        'bower_components/paper-*/*.html',
+        'bower_components/google-*/*.html',
+        'bower_components/gold-*/*.html',
+        'bower_components/neon-*/*.html',
+        'bower_components/platinum-*/*.html',
+        'bower_components/polymer/*.html',
+        'dist/components/*.html'
+    ], {base: './'})
+        .pipe(cache('components'))
+        .pipe(polyclean.cleanCss())
+        .pipe(polyclean.leftAlignJs())
+        .pipe(polyclean.uglifyJs())
+        .pipe(htmlmin({removeComments: true}))
+        .pipe(gulp.dest('./'));
 });
 
-gulp.task('scripts', function () {
+gulp.task('scripts', ['coffee'], function () {
+    return gulp.src(['bower_components/jquery/dist/jquery.js', 'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js'])
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('coffee', function () {
     return gulp.src('src/coffee/*.coffee')
-        .pipe(concat('landing.js'))
         .pipe(coffee())
         .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('css', function () {
     return gulp.src(['src/sass/*.sass'])
-        .pipe(sass({includePaths:['bower_components/bootstrap-sass/assets/stylesheets']}).on('error', sass.logError))
+        .pipe(sass({includePaths: ['bower_components/bootstrap-sass/assets/stylesheets']}).on('error', sass.logError))
         .pipe(postcss([require('postcss-flexibility'), autoprefixer({browsers: ['last 3 versions'], cascade: false})]))
         .pipe(gulp.dest('dist/css'));
 });
@@ -53,4 +78,34 @@ gulp.task('fonts', function () {
     return gulp.src(['src/fonts/*', 'bower_components/bootstrap-sass/assets/fonts/bootstrap/*'])
         .pipe(newer('dist/fonts'))
         .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('components', ['style-modules', 'js-modules', 'scripts'], function () {
+    return gulp.src('src/components/*')
+        .pipe(vulcanize({
+            inlineScripts: true,
+            inlineCss: true,
+            stripComments: true,
+            excludes: ['bower_components/', 'dist/components/']
+        }))
+        .pipe(gulp.dest('dist/components'))
+});
+gulp.task('style-modules', function () {
+    var path = require('path');
+    return gulp.src('src/sass/*.module.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(stylemod({
+            filename: function (file) {
+                return path.basename(file.path, path.extname(file.path));
+            },
+            moduleId: function (file) {
+                return path.basename(file.path, path.extname(file.path));
+            }
+        }))
+        .pipe(gulp.dest('dist/css-modules'));
+});
+gulp.task('js-modules', function () {
+    return gulp.src('src/coffee/*.module.coffee')
+        .pipe(coffee())
+        .pipe(gulp.dest('dist/js-modules'));
 });
--- src/index.php
+++ src/index.php
@@ -5,21 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Bootstrap 101 Template</title>
-
-    <!-- Bootstrap -->
     <link href="/dist/css/style.css" rel="stylesheet">
+
+    <title>Site Template</title>
 
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    
+    <script src="/bower_components/webcomponentsjs/webcomponents.min.js"></script>
     <script src="/bower_components/flexibility/dist/flexibility.js"></script>
+
+    <link rel="import" href="/dist/components/bootstrap.html" async>
 </head>
 <body>
 
-<script src="/bower_components/jquery/dist/jquery.min.js"></script>
-<script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
 </body>
 </html>
