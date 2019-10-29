80c35cb25e65f9dd13f49115a55349a3fa78339d
cimmwolf <cimmwolf@gmail.com>
2019-10-29 12:21:36 +0300

    Move polyfills to separate php-file

--- src/pages/index.php
+++ src/pages/index.php
@@ -5,32 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <script>
-        (function() {
-            if (!('scrollBehavior' in document.documentElement.style)) {
-                let script = document.createElement('script');
-                script.src = '/node_modules/smoothscroll-polyfill/dist/smoothscroll.min.js';
-                document.write(script.outerHTML);
-            }
-            if (!window.HTMLPictureElement) {
-                let script = document.createElement('script');
-                script.src = '/node_modules/picturefill/dist/picturefill.min.js';
-                document.write(script.outerHTML);
-            }
-            let features = [];
-            if (!('remove' in Element.prototype)) {
-                features.push('Element.prototype.remove');
-            }
-            if (!('IntersectionObserver' in window)) {
-                features.push('IntersectionObserver');
-            }
-            if (features.length > 0) {
-                let script = document.createElement('script');
-                script.src = 'https://cdn.polyfill.io/v2/polyfill.min.js?features=' + features.join();
-                document.write(script.outerHTML);
-            }
-        })();
-    </script>
+    <?php require __DIR__ . '/_polyfills.php' ?>
+
     <script src="/node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js"></script>
 
     <link href="/dist/css/style.css" rel="stylesheet">
