1fceda2615a7956de20bb47b5f4645afc6255ca7
cimmwolf <cimmwolf@gmail.com>
2020-03-15 20:48:10 +0400

    Reformat code

--- src/pages/_polyfills.php
+++ src/pages/_polyfills.php
@@ -1,34 +1,34 @@
 <script>
-    (function() {
-        if (!('scrollBehavior' in document.documentElement.style)) {
-            let script = document.createElement('script');
-            script.src = '/node_modules/smoothscroll-polyfill/dist/smoothscroll.min.js';
-            document.write(script.outerHTML);
-        }
-        if (!window.HTMLPictureElement) {
-            let script = document.createElement('script');
-            script.src = '/node_modules/picturefill/dist/picturefill.min.js';
-            document.write(script.outerHTML);
-        }
-        let features = [];
-        if (!('remove' in Element.prototype)) {
-            features.push('Element.prototype.remove');
-        }
-        if (!('IntersectionObserver' in window)) {
-            features.push('IntersectionObserver');
-        }
-        if (features.length > 0) {
-            let script = document.createElement('script');
-            script.src = 'https://cdn.polyfill.io/v2/polyfill.min.js?features=' + features.join();
-            document.write(script.outerHTML);
-        }
-    })();
+	(function() {
+		if (!('scrollBehavior' in document.documentElement.style)) {
+			let script = document.createElement('script');
+			script.src = '/node_modules/smoothscroll-polyfill/dist/smoothscroll.min.js';
+			document.write(script.outerHTML);
+		}
+		if (!window.HTMLPictureElement) {
+			let script = document.createElement('script');
+			script.src = '/node_modules/picturefill/dist/picturefill.min.js';
+			document.write(script.outerHTML);
+		}
+		let features = [];
+		if (!('remove' in Element.prototype)) {
+			features.push('Element.prototype.remove');
+		}
+		if (!('IntersectionObserver' in window)) {
+			features.push('IntersectionObserver');
+		}
+		if (features.length > 0) {
+			let script = document.createElement('script');
+			script.src = 'https://cdn.polyfill.io/v2/polyfill.min.js?features=' + features.join();
+			document.write(script.outerHTML);
+		}
+	})();
 </script>
 <script src="/node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js" defer></script>
 <script type="module">
-    window.WebComponents = window.WebComponents || {
-        waitFor(cb) {
-            addEventListener('WebComponentsReady', cb);
-        },
-    };
+	window.WebComponents = window.WebComponents || {
+		waitFor(cb) {
+			addEventListener('WebComponentsReady', cb);
+		},
+	};
 </script>
