e38bc56552b8b78b2e2d9d3c09cb57562837e7f4
cimmwolf <cimmwolf@gmail.com>
2020-03-15 20:33:00 +0400

    Change link to styles

--- src/pages/index.php
+++ src/pages/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="/dist/css/style.css" rel="stylesheet">
+    <link href="/src/sass/style.sass" rel="stylesheet">
 
     <?php require __DIR__ . '/_favicon.php' ?>
     <?php require __DIR__ . '/_polyfills.php' ?>
