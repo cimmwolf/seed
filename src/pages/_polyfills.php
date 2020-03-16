c3c16c292dc12ca642a4cb1b80398f7b209786f1
cimmwolf <cimmwolf@gmail.com>
2020-03-16 23:10:51 +0400

    Remove picturefill polyfill

--- src/pages/_polyfills.php
+++ src/pages/_polyfills.php
@@ -5,15 +5,7 @@
 			script.src = '/node_modules/smoothscroll-polyfill/dist/smoothscroll.min.js';
 			document.write(script.outerHTML);
 		}
-		if (!window.HTMLPictureElement) {
-			let script = document.createElement('script');
-			script.src = '/node_modules/picturefill/dist/picturefill.min.js';
-			document.write(script.outerHTML);
-		}
 		let features = [];
-		if (!('remove' in Element.prototype)) {
-			features.push('Element.prototype.remove');
-		}
 		if (!('IntersectionObserver' in window)) {
 			features.push('IntersectionObserver');
 		}
